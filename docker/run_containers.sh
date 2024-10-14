3bf2c348a75b92e08493c1af90257ab2594d2351
Yi Hong-ju <to_hongju@pm.me>
2024-10-14 07:52:52 +0000

    Update 'docker' module again
    - docker/Dockerfile: CMD 추가: Detached 모드에 대응하기 위함 (백그라운드 실행)
    - docker/run_containers.sh: docker build, run 커맨드 수정

--- docker/run_containers.sh
+++ docker/run_containers.sh
@@ -3,14 +3,13 @@
 NUM_CONTAINERS=10
 SHM_SIZE=16g
 
-docker build -t yihongju/ddal-kkak:0.1 ./docker
+docker build -t yihongju/ddal-kkak:0.1 ./docker --no-cache
 
 for n in $(seq 0 $((NUM_CONTAINERS - 1)))
 do
-docker run -it \
+docker run -d \
     --name ddal-kkak-$n \
     --gpus all \
     --shm-size=$SHM_SIZE \
-    yihongju/ddal-kkak:0.1 \
-    bash
+    yihongju/ddal-kkak:0.1
 done
