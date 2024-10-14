2c04774f1f1b086685c51caee0a2a3d4ede51eb5
Yi Hong-ju <to_hongju@pm.me>
2024-10-14 08:06:35 +0000

    Fix wrong Dockerfile path

--- docker/run_containers.sh
+++ docker/run_containers.sh
@@ -2,8 +2,10 @@
 
 NUM_CONTAINERS=10
 SHM_SIZE=16g
+DDAL_KKAL_HOME="/usr/local/ddal-kkak"
 
-docker build -t yihongju/ddal-kkak:0.1 ./docker --no-cache
+docker build -t yihongju/ddal-kkak:0.1 \
+    $DDAL_KKAL_HOME/docker --no-cache
 
 for n in $(seq 0 $((NUM_CONTAINERS - 1)))
 do
